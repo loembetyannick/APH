f6308f6e964064de35ba82b65aedb6c44b149789
loembetyannick <loembetyannick@outlook.com>
2022-10-04 23:45:19 +0100

    fixing-small mission changes

--- aph/src/Component/Mission.js
+++ aph/src/Component/Mission.js
@@ -5,7 +5,7 @@ import MissionStep from '../features/MissionStep'
 import no1 from '../APH-IMAGES/no1.svg'
 import no2 from '../APH-IMAGES/no2.svg'
 import no3 from '../APH-IMAGES/no3.svg'
-import {text_data} from '../features/TextData.js'
+import {mission_text_data} from '../features/MissionTextData.js'
 
 const Mission = () => {
   return (
@@ -15,9 +15,9 @@ const Mission = () => {
         <div className='m-item-container'>
           <p className='m-item-text'>ADAL Plumbing & Heating’s mission is to be one of the best Doncaster-based Plumbing & Heating contractors. We will earn customers’ respect and trust by doing what we do best by providing excellent value for money service. </p>
         </div>
-        <MissionStep image={no1} text={text_data[0].text}/>
-        <MissionStep image={no2} text={text_data[1].text}/>
-        <MissionStep image={no3} text={text_data[2].text}/>
+        <MissionStep image={no1} text={mission_text_data[0].text}/>
+        <MissionStep image={no2} text={mission_text_data[1].text}/>
+        <MissionStep image={no3} text={mission_text_data[2].text}/>
       </div>
     </div>
   )
---
+++ aph/src/features/MissionTextData.js
@@ -0,0 +1,12 @@
+
+export const mission_text_data = [
+    {
+        text: "Provide an effective and reliable service, delivered with high-quality workmanship.",
+    },
+    {
+        text: "Provide our customers with a value for money product making sure the quality is kept to a high standard.",
+    },
+    {
+        text: "3. Build and establish lasting relationships with our customers.",
+    }
+]
